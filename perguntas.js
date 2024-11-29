54848ed93ad7ef57e6d2005d84e037130c3ff430
Pamonha0964 <97123521+Pamonha0964@users.noreply.github.com>
2024-11-28 22:57:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um Banco de Dados?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Qual a tradução de "What should they have done?" para português?',
+    'O que eles deveriam ter feito?'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o elemento químico mais abundante no universo?',
+    'O elemento químico mais abundante no universo é o hidrogênio (H)'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a formula de Bhaskara?',
+    'A fórmula de Bhaskara é dada por: x = (-b ± √(b² - 4ac)) / (2a)'
+)
